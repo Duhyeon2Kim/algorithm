1e24feffe746165e2c6350d797b4d44432f2db58
Duhyeon2Kim <122500529+Duhyeon2Kim@users.noreply.github.com>
2023-03-26 21:32:02 +0900

    [Silver II] Title: N과 M (9), Time: 684 ms, Memory: 54116 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N,M;
+	static int[] arr;
+	static int[] result;
+	static boolean[] visit;
+	static LinkedHashSet<String> list;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()) ;
+		N=Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		arr = new int[N];
+		result = new int[M];
+		visit = new boolean[N];
+		list = new LinkedHashSet<>();
+		StringTokenizer st2 = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) {
+			arr[i]= Integer.parseInt(st2.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		recursive(0);
+		for(String x: list) {
+			System.out.println(x);
+		}
+		
+	}
+	private static void recursive(int idx) {
+		if(idx == M) {
+			String temp="";
+			for(int i=0; i<M; i++) {
+				temp+=result[i]+" ";
+			}
+			{list.add(temp);}
+			return;
+		}
+		for(int i=0; i<N; i++) {
+			if(!visit[i]) {
+				visit[i]=true;
+				result[idx]=arr[i];
+				recursive(idx+1);
+				visit[i]=false;	
+			}
+		}
+	}
+}
