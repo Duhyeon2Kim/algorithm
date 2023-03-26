d43fdb972bdf1c1c593b1f77c65623c7fe135efd
Duhyeon2Kim <122500529+Duhyeon2Kim@users.noreply.github.com>
2023-03-26 19:55:34 +0900

    [Silver III] Title: N과 M (1), Time: 208 ms, Memory: 50036 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,40 +1,39 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	static int N, M;
-	static boolean[] check;
+	static int N,M;
 	static int[] arr;
-
-	private static void dfs(int idx) {
-		if (idx == M) {
-			for (int i = 0; i < M; i++) {
-				System.out.print(arr[i] + " ");
-			}
-			System.out.println();
-			return;
+	static StringBuilder sb = new StringBuilder();
+	static boolean[]visit;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()) ;
+		N=Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		visit = new boolean[N];
+		arr= new int[M];
+		recursive(0);
+		System.out.println(sb);
+		
+		
+	}
+	private static void recursive(int idx) {
+		if(idx==M) {
+			for(int i=0; i<M; i++) {
+				sb.append(arr[i]+" ");
+			}sb.append("\n"); return;
 		}
 		
-		for (int i = 1; i <= N; i++) {
-			if (check[i] == false) {
-				check[i] = true;
-				arr[idx] = i;
-				dfs(idx + 1);
-				check[i] = false;
-			}
+		for(int i=0; i<N; i++) {
+			if(!visit[i]) {
+				visit[i]=true;
+				arr[idx]=i+1;
+			recursive(idx+1);
+			visit[i]=false;}
+			
 		}
-
-
-
-	}
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		M = sc.nextInt();
-
-		check = new boolean[N + 1];
-		arr = new int[M+1];
-
-		dfs(0);
 	}
 }
