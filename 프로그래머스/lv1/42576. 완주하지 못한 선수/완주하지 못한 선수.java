24dbcf5ea2ae18ff6e5565ca2258d69e208a1ebd
Duhyeon2Kim <engus5860@gmail.com>
2023-06-13 20:02:00 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 69.78 ms, Memory: 95.5 MB -BaekjoonHub

--- 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,14 +1,20 @@
 import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
-        Arrays.sort(participant);
-        Arrays.sort(completion);
-        for(int i=0; i<completion.length; i++) {
-            if(!participant[i].equals(completion[i])) {
-                return participant[i];
+        HashMap<String,Integer> map = new HashMap<>();
+        for(String player : participant) {
+            map.put(player,map.getOrDefault(player,0)+1);
+        }
+        for (String player : completion) {
+            map.put(player, map.get(player) - 1);
+        }
+
+        for (String key : map.keySet()) {
+            if(map.get(key)!=0) {
+                return key;
             }
         }
-        return participant[completion.length];
+        
+        return "hi";
     }
 }
